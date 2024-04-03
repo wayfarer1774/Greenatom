229d865370af6c9eacbb373859f058c680c8d18b
Alexander <alex53291@gmail.com>
2024-04-03 12:46:32 +0300

    Solution

--- src/main/java/com/Alexander/Task/FindMissingNumber.java
+++ src/main/java/com/Alexander/Task/FindMissingNumber.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class FindMissingNumber {
     public static void main(String[] args) {
+        //Ввод количества чисел в оригинальной последовательности и последовательности с "потерянным" числом.
         System.out.println("Введите количество чисел в массиве.");
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
@@ -13,6 +14,7 @@ public class FindMissingNumber {
             sequence[i]=scanner.nextInt();
         }
 
+        //Применение операции побитового исключающего ИЛИ (XOR) для выявления "потерянного" числа.
         int totalXOR = 0;
         for (int i = 1; i <= N; i++) {
             totalXOR ^= i;
